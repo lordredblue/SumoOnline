25bbdd3b4258a5cbeaf66de2ed3fd6133baedba4
shastrihm <32887722+shastrihm@users.noreply.github.com>
2018-01-11 19:51:01 -0800

    Update spaceScript.js

--- Sumogame/public/spaceScript.js
+++ Sumogame/public/spaceScript.js
@@ -8,9 +8,6 @@ var toolCanvas = document.getElementById("toolbar");
 var toolCtx = toolCanvas.getContext("2d");
 
 
-
-
-
 //initialize images 
 var sumoImg = new Image();
 var sumos_y = 0;
@@ -38,11 +35,11 @@ readyin1Img.src = "images/readyin1.jpg";
 var fightImg = new Image();
 fightImg.src = "images/Fight_1.png";
 
+var barFill = new Image();
+barFill.src = "images/Bar_Fill.png";
 
-
-
-
-
+var barBG = new Image();
+barBG.src = "images/Bar_BG.png";
 
 var requestAnimationFrame = window.requestAnimationFrame
 var pos_x = 0;
@@ -59,14 +56,15 @@ function animateFight()
     requestAnimationFrame(animateFight);
 }
   else{
+  	ctx.font = "70px Joti One";
     ctx.clearRect(0,0,canvas.width, canvas.height);
     ctx.drawImage(sumoImg, 0,sumos_y, canvas.width, canvas.height);
-    ctx.font = "70px Joti One";
 	ctx.fillStyle = "red";
 	ctx.fillText("Red: " +  String(spaceCounter), 10, 70);
 	ctx.fillStyle = "blue";
 	ctx.fillText("Blue: " + String(enterCounter), 1100, 70);
 	gameStart = true;
+	animateTimer();
   }
 }
 
@@ -105,18 +103,18 @@ function animateDuringGame()
 function changeBorderColor()
 	{
 		if(sumos_x < 0){
-			canvas.style.border = "5px solid #004cff";
+			canvas.style.border = "5px dashed #004cff";
 
 		};
 
 		if(sumos_x > 0){
-			canvas.style.border = "5px solid #ff2e00";
+			canvas.style.border = "5px dashed #ff2e00";
 		};
 
 		if(sumos_x == 0){
-			canvas.style.border = "5px solid #a500ff";
+			canvas.style.border = "5px dashed #000000";
 		};
-		toolCanvas.style.border = canvas.style.border;
+		//toolCanvas.style.border = canvas.style.border;
 	};
 	
 //Spits out the result depending on who wins
@@ -190,33 +188,36 @@ function compareSpaceEnter()
 	}
 };
 
-//Timer
-function countDown(secs, elem)
-{
-	var element = document.getElementById(elem);
-	if(secs <= 10){
-		element.innerHTML = "Time remaining: " + secs + " seconds";
-	};
+var reduce = toolCanvas.width;
 
-	secs--;
-	var timer = setTimeout('countDown('+secs+',"'+elem+'")', 1000);
-	
-	if(secs < -1){
-		clearTimeout(timer);
-		element.innerHTML = '<h3>Game Over!</h3>'
-		compareSpaceEnter();
-		gameEnd = true;
-		//code for what happens after countdown ends goes here
-	};
-	
-};
+function animateTimer()
+{
+	 reduce-=2.088;
+	 if(reduce-83-110>= 0){
+		  toolCtx.clearRect(0,0,toolCanvas.width-110,toolCanvas.height);
+		  toolCtx.drawImage(barBG,0,0,toolCanvas.width-110, toolCanvas.height);
+		  toolCtx.drawImage(barFill,60,9,reduce-83-110, toolCanvas.height-21);
+		     window.requestAnimationFrame(animateTimer);
+		 }
+	else{
+	  toolCtx.clearRect(0,0,toolCanvas.width-110,toolCanvas.height);
+	  toolCtx.drawImage(barBG,0,0,toolCanvas.width-110, toolCanvas.height);
+	  toolCtx.drawImage(barFill,60,9,0, toolCanvas.height-21);
+	  toolCtx.font = "40px Joti One";
+	  toolCtx.fillStyle = "white";
+	  toolCtx.fillText("Game Over!", toolCanvas.width/2 - 120,toolCanvas.height/2+15);
+	  compareSpaceEnter();
+	  gameEnd = true;
+	}
+}
+
 
 //plays the game
 function playGame()
 {	
 	loadInitialCanvas();
 	window.addEventListener("keyup", checkKeyPressed, false);
-	countDown(14, "underCanvas"); //add x to desired time limit to allow for 123fight
+	
 };
 
 
